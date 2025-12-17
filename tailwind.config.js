2caf05c945d3498435823c9bf58d6c2d7ab1f321
Tolulope Ebenezer <stnezer@icloud.com>
2025-12-17 19:59:53 +0100

    staff section tailwindcss edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'max-1167': { max: '1167px' },
+      },
+    },
+  },
   content: [
     "./index.html",
     "./src/**/*.{html,js,ts,jsx,tsx}",
